3dcb9bdc858bf9f95bdbf25f1eeaef6f0b9fc333
UliGarci <20203tn022@utez.edu.mx>
2023-12-08 00:55:42 -0600

    Rama de administración para el administrador

--- web-jewelry/src/routes/PublicRoutes.js
+++ web-jewelry/src/routes/PublicRoutes.js
@@ -19,19 +19,12 @@ export const PublicRoutes = () => {
 
             <Routes>
                 <Route path="home" element={<HomeProducts/>}/>
-                <Route path="login/*" element={
-                    <PublicRoute>
-                        <Routes>
-                            <Route path="*" element={<LoginPage/>}/>
-                            <Route path="register" element={<Register/>}/>
-                            <Route path="recover" element={<RecoverPasswordPage/>}/>
-                        </Routes>
-                    </PublicRoute>
-                }/>
+                <Route path="login" element={<LoginPage/>}/>
+                <Route path="login/register" element={<Register/>}/>
+                <Route path="login/recover" element={<RecoverPasswordPage/>}/>
                 <Route path="product"/>
                 <Route path="product/:id" element={<ProductPage/>}/>
-                <Route path="/" element={<Navigate to={"/home"} />}/>
-                <Route path="*" element={<div>404</div>}/>
+                <Route path="*" element={<Navigate to={"/home"} />}/>
             </Routes>
         </UserProviderCart>
     )
--- web-jewelry/src/users/admin/pages/Supplier.js
+++ web-jewelry/src/users/admin/pages/Supplier.js
@@ -1,13 +1,16 @@
-import React, { useEffect,useState } from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import Table from 'react-bootstrap/Table';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import {Container} from "react-bootstrap";
+import {AuthContext} from "../../../auth";
 
 export const Supplier = () => {
-    // USESTATE PARA ESTADO DE MODAL
+    // usestate para estado de modal editar
     const [showModalEdit, setShowModalEdit] = useState(false);
+    // usestate para estado de modal agregar
+    const [showModalAdd,setShowModalAdd]=useState(false);
     // USESTATE PARA MODIFICAR EL TITULO DEL MODAL
     const [titleModal, setTitleModal] = useState('')
     // USESTATE PARA ALMACENAR LA INFORMACIÓN DEL PROVEEDOR
@@ -15,30 +18,124 @@ export const Supplier = () => {
     // USESTATE PARA ALMACENAR PROVEEDORES
     const [proveedores, setProveedores] = useState([]);
 
-    const url_api="http://localhost:8080/api/suppliers"
+    const url_api="http://localhost:8080/api/suppliers/"
+    const {user} = useContext(AuthContext);
 
     useEffect(() => {
-        fetch(url_api)
-            .then((res) =>
-                res.json()
-            ).then((resp) => {
-                console.log("Respuesta:",resp)
-            setProveedores(resp)
-        }).catch((error) => {
-            console.log("Error:",error);
-        })
+       fetch(url_api,{
+           method:"GET",
+           headers:{
+               'Content-Type':'application/JSON',
+               'Authorization':`Bearer ${user?.token}`
+           }
+       })
+           .then((resp)=>resp.json())
+           .then((data)=>{
+               if(data.statusCode===200){
+                   setProveedores(data.data);
+               }
+           })
+           .catch((err)=>console.log("Error: ",err));
     }, [])
 
     // funciones para abrir y cerrar modal
     const handleClose = () => {
         setShowModalEdit(false);
         setProveedorSeleccionado(null);
+        setShowModalAdd(false);
     };
     const handleShow = (title, proveedor) => {
-        setShowModalEdit(true);
         setTitleModal(title);
+        setShowModalEdit(true);
         setProveedorSeleccionado(proveedor)
     };
+
+    // función para actualizar proveedor
+    const saveupdate=()=>{
+        const {status,...supplierdata}=proveedorSeleccionado
+        const updateSupplier ={
+            ...supplierdata,
+            status:{id:status.id}
+        }
+        console.log("STATUS: ",updateSupplier)
+        fetch(url_api,{
+            method:'PUT',
+            headers:{
+                'Content-Type':'application/JSON',
+                'Authorization':`Bearer ${user?.token}`
+            },
+            body:JSON.stringify(updateSupplier)
+        }).then((resp)=>resp.json())
+            .then((data)=>{
+                if(data.statusCode===200){
+                    console.log("EXITO")
+                    getnewsupplier()
+                }else if(data.error===true){
+                    console.log("Error en saveupdate():")
+                }
+            })
+    }
+
+    const getnewsupplier=()=>{
+        fetch(url_api,{
+            method:"GET",
+            headers:{
+                'Content-Type':'application/json',
+                'Authorization':`Bearer ${user?.token}`
+            }
+        })
+            .then((resp)=>resp.json())
+            .then((data)=>{
+                if(data.statusCode===200){
+                    setProveedores(data.data);
+                }
+            })
+            .catch((err)=>console.log("Error: ",err));
+    }
+
+    const openModalAdd=()=>{
+        setShowModalAdd(true);
+        setProveedorSeleccionado({
+            "name": "",
+            "rfc": "",
+            "physical_address": "",
+            "branch_address": "",
+            "email": "",
+            "phone_number": "",
+            "status": {
+                "id": 1
+            }
+        })
+    }
+
+    // función para registrar proveedor
+    const savenew=()=>{
+        console.log("new:",proveedorSeleccionado)
+        fetch(url_api,{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/JSON',
+                'Authorization':`Barer ${user?.token}`
+            },
+            body:JSON.stringify(proveedorSeleccionado)
+        }).then((resp)=>resp.json())
+            .then((data)=>{
+                if(data.statusCode===200){
+                    console.log("EXITO")
+                }else if(data.error===true){
+                    console.log("Error en savenew()")
+                }
+            }).catch((err)=>console.log("Error: ",err))
+    }
+
+    //función para manejar el switch del status del proveedor en el modal
+    const changeStatus=(e)=>{
+        const newStatus = e.target.checked?1:2;
+        setProveedorSeleccionado({
+            ...proveedorSeleccionado,
+            status:{id:newStatus}
+        })
+    }
 
     return (
         <Container style={{height: "90vh", overflowY: "hidden"}}>
@@ -46,32 +143,37 @@ export const Supplier = () => {
                 <h1>Lista de proveedores</h1>
             </div>
             <div style={{ padding: '20px', display: 'flex', justifyContent: 'flex-end' }}>
-                <button onClick={() => handleShow('Agregar proveedor')} className='btn btn-success'>Agregar</button>
+                <button onClick={() => openModalAdd()} className='btn btn-success'>Agregar</button>
             </div>
             {/* TABLA DE PROVEEDORES */}
             <div style={{ overflowY: 'auto', height: '70vh' }}>
                 <Table striped bordered hover size="sm">
                     <thead>
-                    <tr>
+                    <tr style={{textAlign:'center'}}>
                         <th style={{ backgroundColor: '#D1D1D1' }}>#</th>
                         <th style={{ backgroundColor: '#D1D1D1' }}>Nombre del proveedor</th>
-                        <th style={{ backgroundColor: '#D1D1D1' }}>Número de telefono</th>
+                        <th style={{ backgroundColor: '#D1D1D1' }}>RFC</th>
+                        <th style={{ backgroundColor: '#D1D1D1' }}>Dirección</th>
+                        <th style={{ backgroundColor: '#D1D1D1' }}>Número de teléfono</th>
                         <th style={{ backgroundColor: '#D1D1D1' }}>correo electrónico</th>
+                        <th style={{ backgroundColor: '#D1D1D1' }}>Estatus</th>
                         <th style={{ backgroundColor: '#D1D1D1' }}>Acciones</th>
                     </tr>
                     </thead>
                     <tbody>
                     {proveedores.map((proveedor,index) => (
                         <tr key={index}>
-                            <td>{proveedor.index}</td>
+                            <td>{index+1}</td>
                             <td>{proveedor.name}</td>
+                            <td>{proveedor.rfc}</td>
+                            <td>{proveedor.physical_address},{proveedor.branch_address}</td>
                             <td>{proveedor.phone_number}</td>
                             <td>{proveedor.email}</td>
+                            <td>{proveedor.status.description}</td>
                             <td>
                                 <button onClick={() => handleShow('Editar información de proveedor', proveedor)} style={{ margin: '10px 8px 10px 10px' }} className='btn btn-primary'>
                                     Editar
                                 </button>
-                                <button className='btn btn-danger'>Eliminar</button>
                             </td>
                         </tr>
                     ))}
@@ -86,14 +188,17 @@ export const Supplier = () => {
                 </Modal.Header>
                 <Modal.Body>
                     <div>
+                        <div style={{display:'flex',justifyContent:'end'}}>
+                            <Form.Switch checked={!proveedorSeleccionado || proveedorSeleccionado?.status?.id ===1} onChange={changeStatus}/>
+                        </div>
                         <Form.Label>Nombre del proveedor</Form.Label>
-                        <Form.Control type='text' value={proveedorSeleccionado?.name || ''} />
+                        <Form.Control type='text' value={proveedorSeleccionado?.name || ''} onChange={(e)=>setProveedorSeleccionado({...proveedorSeleccionado,name:e.target.value})}/>
                         <br />
                         <Form.Label>Número de teléfono</Form.Label>
-                        <Form.Control type="tel" value={proveedorSeleccionado?.phone || ''} />
+                        <Form.Control type="tel" value={proveedorSeleccionado?.phone_number || ''} onChange={(e)=>setProveedorSeleccionado({...proveedorSeleccionado,phone_number:e.target.value})}/>
                         <br />
                         <Form.Label>Correo electrónico</Form.Label>
-                        <Form.Control type='email' value={proveedorSeleccionado?.email || ''} />
+                        <Form.Control type='email' value={proveedorSeleccionado?.email || ''} onChange={(e)=>setProveedorSeleccionado({...proveedorSeleccionado,email:e.target.value})}/>
                         <br />
                     </div>
                 </Modal.Body>
@@ -101,7 +206,54 @@ export const Supplier = () => {
                     <Button variant="secondary" onClick={handleClose}>
                         Cancelar
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
+                    <Button variant="primary" onClick={()=>{
+                        handleClose();
+                        saveupdate();
+                    }}>
+                        Guardar cambios
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            {/* modal agregar */}
+            <Modal style={{ margin: 'auto', top: '5vh', height:'90vh' }} show={showModalAdd} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Agregar proveedor</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div>
+                        <div style={{display:'flex',justifyContent:'end'}}>
+                            <Form.Switch checked={!proveedorSeleccionado || proveedorSeleccionado?.status?.id ===1} onChange={changeStatus}/>
+                        </div>
+                        <Form.Label>Nombre del proveedor</Form.Label>
+                        <Form.Control type='text' value={proveedorSeleccionado?.name || ''} onChange={(e)=>setProveedorSeleccionado({...proveedorSeleccionado,name:e.target.value})}/>
+                        <br />
+                        <Form.Label>Número de teléfono</Form.Label>
+                        <Form.Control type="tel" value={proveedorSeleccionado?.phone_number || ''} onChange={(e)=>setProveedorSeleccionado({...proveedorSeleccionado,phone_number:e.target.value})}/>
+                        <br />
+                        <Form.Label>Correo electrónico</Form.Label>
+                        <Form.Control type='email' value={proveedorSeleccionado?.email || ''} onChange={(e)=>setProveedorSeleccionado({...proveedorSeleccionado,email:e.target.value})}/>
+                        <br />
+                        <Form.Label>RFC</Form.Label>
+                        <Form.Control type='text' value={proveedorSeleccionado?.rfc || ''} onChange={(e)=>setProveedorSeleccionado({...proveedorSeleccionado,rfc:e.target.value})}/>
+                        <br />
+                        <Form.Label>Dirreción de la empresa</Form.Label>
+                        <Form.Control type='text' value={proveedorSeleccionado?.branch_address || ''} onChange={(e)=>setProveedorSeleccionado({...proveedorSeleccionado,branch_address:e.target.value})}/>
+                        <br />
+                        <Form.Label>Dirección del negocio</Form.Label>
+                        <Form.Control type='text' value={proveedorSeleccionado?.physical_address || ''} onChange={(e)=>setProveedorSeleccionado({...proveedorSeleccionado,physical_address:e.target.value})}/>
+                        <br />
+
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Cancelar
+                    </Button>
+                    <Button variant="primary" onClick={()=>{
+                        handleClose();
+                        savenew()
+                    }}>
                         Guardar cambios
                     </Button>
                 </Modal.Footer>
